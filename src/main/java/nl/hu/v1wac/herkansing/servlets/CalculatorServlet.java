1e121a3706ce29cd0f02c7dca3837890d5fa693c
thomasboy020 <thomasboy020@gmail.com>
2018-05-29 14:53:15 +0200

    calcu

--- src/main/java/nl/hu/v1wac/herkansing/servlets/CalculatorServlet.java
+++ src/main/java/nl/hu/v1wac/herkansing/servlets/CalculatorServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = "/Calculator")
+@WebServlet(urlPatterns = "/Rekenmachine")
 public class CalculatorServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException,IOException {
